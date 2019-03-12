c559b019a705a02d99cc7fbdb4568d868e602af9
Vasily <post_lv@mail.ru>
2019-03-12 21:40:14 +0300

    add form app

---
+++ gtkApp/MessageBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+public static class MessageBox
+{
+    public static void Show(Gtk.Window parent_window, DialogFlags flags, MessageType msgtype, ButtonsType btntype, string msg)
+    {
+        MessageDialog md = new MessageDialog(parent_window, flags, msgtype, btntype, msg);
+        md.Run();
+        md.Destroy();
+    }
+    public static void Show(string msg)
+    {
+        MessageDialog md = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, msg);
+        md.Run();
+        md.Destroy();
+    }
+}
+
