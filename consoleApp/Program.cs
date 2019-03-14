82a126adc37a8c27148c3e93f7fa2c6a8c660375
Vasily <post_lv@mail.ru>
2019-03-14 21:51:58 +0300

    internal class

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -2,13 +2,28 @@
 
 namespace consoleApp
 {
+    class BaseClass
+    {
+
+        public class InternalClass
+        {
+            public int Number { get; set; }
+        }
+
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
             MyClass instance = new MyClass();
             instance.Method1();
-            instance.Method2(); 
+            instance.Method2();
+
+            BaseClass.InternalClass intClass = new BaseClass.InternalClass();
+            intClass.Number = 5;
+
+            Console.WriteLine(intClass.Number);
         }
     }
 }
